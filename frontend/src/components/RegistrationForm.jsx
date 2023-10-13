59c0cc52677bb881f2351306f23058f80c00eff1
hritik <hritik2805@gmail.com>
2023-10-13 12:40:36 +0530

    registation form ui

---
+++ frontend/src/components/RegistrationForm.jsx
@@ -0,0 +1,92 @@
+import { Button, Stack, TextField } from "@mui/material";
+import React, { useState } from "react";
+
+const RegistrationForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handleSubmit = () => {};
+
+  return (
+    <div className="form">
+      <h2> Registration Form</h2>
+      <form onSubmit={handleSubmit}>
+        <Stack spacing={2} direction="row" sx={{ marginBottom: 4 }}>
+          <TextField
+            type="text"
+            variant="outlined"
+            color="secondary"
+            label="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            fullWidth
+            required
+          />
+          <TextField
+            type="text"
+            variant="outlined"
+            color="secondary"
+            label="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            fullWidth
+            required
+          />
+        </Stack>
+        <TextField
+          type="email"
+          variant="outlined"
+          color="secondary"
+          label="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          fullWidth
+          required
+          sx={{ mb: 4 }}
+        />
+        <TextField
+          type="password"
+          variant="outlined"
+          color="secondary"
+          label="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          fullWidth
+          required
+          sx={{ mb: 2 }}
+        />
+        <TextField
+          type="password"
+          variant="outlined"
+          color="secondary"
+          label="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          fullWidth
+          required
+          sx={{ mb: 2 }}
+        />
+        <TextField
+          type="number"
+          variant="outlined"
+          color="secondary"
+          label="Phone Number"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+          fullWidth
+          required
+          sx={{ mb: 2 }}
+        />
+        <Button variant="outlined" color="secondary" type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default RegistrationForm;
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import Header from '../components/Header'
-
+import React from "react";
+import Header from "../components/Header";
+import RegistrationForm from "../components/RegistrationForm";
+import "./SignUp.css";
 const SignUp = () => {
   return (
     <div>
-      <Header/>
-      
+      <Header />
+      <div className="form-container">
+        <RegistrationForm />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
